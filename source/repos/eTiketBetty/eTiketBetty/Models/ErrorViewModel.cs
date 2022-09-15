61a9c140f9b6e7e6a6efb0b98d37b1249267e314
DESKTOP-C41621Q\betty <bettyindrawati4@gmail.com>
2022-09-15 20:12:44 +0700

    FirstCommit

---
+++ source/repos/eTiketBetty/eTiketBetty/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTiketBetty.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
